e0659f1609754ad37f8bfb8b94659b3a2c9ec302
Sophia Pignatelli <pignatelli@fordham.edu>
2017-03-16 17:12:15 -0400

    use tables to show question and answer content, add form for answers

---
+++ app/controllers/answer.rb
@@ -0,0 +1,17 @@
+
+get '/questions/:id/answers/new' do
+  @question = Question.find_by(id: params[:id])
+  erb :'/questions/show'
+end
+post '/questions/:id/answers' do
+  @question = Question.find_by(id: params[:id])
+  @answer = Answer.new(params[:answer])
+  @answer.question_id = params[:id]
+  @answer.user_id = session[:user_id]
+  if @answer.save
+    redirect "/questions/#{params[:id]}"
+  else
+    @errors = @answer.errors.full_messages
+    erb :'/questions/show'
+  end
+end
