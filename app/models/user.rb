a410c136204465c8a85286d0985ca7f9086235e9
petermrecords <email": null>
2017-03-16 16:29:12 -0400

    added reputation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,16 @@ class User < ActiveRecord::Base
 
   has_many :questions
   has_many :answers
+  has_many :answered_questions, through: :answers, source: :question
   has_many :comments
   has_many :votes
 
   def people_reached
-    self.questions.map { |question| question.views }.inject(:+) + self.answers.map { |answer| answer.question.views }.inject(:+)
+    (self.questions + self.answered_questions).map { |question| question.views }.inject(:+)
+  end
+
+  def reputation
+    binding.pry
+    (self.questions.map { |question| question.votes.map { |vote| vote.up_down }.inject(:+) }.inject(:+)) + (self.answers.map { |answer| answer.votes.map { |vote| vote.up_down }.inject(:+) }.inject(:+))
   end
 end
