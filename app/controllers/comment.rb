12934ab49525aaa91160deaa893f56d3a2909314
Sophia Pignatelli <pignatelli@fordham.edu>
2017-03-17 11:45:04 -0400

    add routes for answer comments

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,7 +1,7 @@
 get '/questions/:id/comments/new' do
   @question = Question.find_by(id: params[:id])
 
-  erb :"/comments/new"
+  erb :"/comments/new_on_question"
 end
 
 post '/questions/:id/comments' do
@@ -16,3 +16,26 @@ post '/questions/:id/comments' do
     erb :"/questions/show"
   end
 end
+
+
+get '/questions/:id/answers/:id/comments/new' do
+  question = Question.find_by(id: params[:id])
+  @answer = Answer.find_by(id: params[:id])
+
+  erb :"/comments/new_on_answer"
+end
+
+post '/questions/:id/answers/:id/comments' do
+  question = Question.find_by(id: params[:id])
+  @answer = Answer.find_by(id: params[:id])
+  @comment = @answer.comments.new(params[:comment])
+  @comment.user_id = session[:user]
+
+  if @comment.save
+    redirect "/questions/#{question.id}"
+  else
+    @errors = @comment.errors.full_messages
+    erb :"/questions/show"
+  end
+end
+
