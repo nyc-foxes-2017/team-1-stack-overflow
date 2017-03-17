ae0a4487e5cf78b314507425c45bc285fc426a6e
Michael Benson <email": null>
2017-03-17 11:34:42 -0400

    thomas stuff

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#new_answer").on("submit", "#answers", function(event){
+    event.preventDefault();
+    url = $(this).attr('action');
+    data = $(this).serialize();
+    $.ajax({
+      url: url,
+      method: 'post',
+      data: data
+    }).done(function(response){
+      $('.answer-container').append(response);
+      $('#answers').trigger("reset");
+    })
+  })
 });
